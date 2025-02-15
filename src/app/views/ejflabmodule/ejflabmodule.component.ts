8d356fa165ac4f0878e02ed992c8af0600627211
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-02-15 10:58:53 -0500

    ok

--- src/app/views/ejflabmodule/ejflabmodule.component.ts
+++ src/app/views/ejflabmodule/ejflabmodule.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { BaseComponent, OptionData } from 'ejflab-front-lib';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService, BackendPageService, BaseComponent, CallService, FileService, FlowchartService, ModalService, OptionData, TupleService, WebcamService } from 'ejflab-front-lib';
 
 @Component({
   selector: 'app-ejflabmodule',
@@ -9,6 +12,50 @@ import { BaseComponent, OptionData } from 'ejflab-front-lib';
 export class EjflabmoduleComponent extends BaseComponent implements OnInit {
 
   public extraOptions: Array<OptionData> = [];
+
+  constructor(
+    public override flowchartSrv: FlowchartService,
+    public override callService: CallService,
+    public override route: ActivatedRoute,
+    public override pageService: BackendPageService,
+    public override cdr: ChangeDetectorRef,
+    public override authService: AuthService,
+    public override dialog: MatDialog,
+    public override tupleService: TupleService,
+    public override fileService: FileService,
+    public override modalService: ModalService,
+    public override webcamService: WebcamService,
+    public override auth: Auth,
+    private router: Router,
+  ) {
+    super(
+      flowchartSrv,
+      callService,
+      route,
+      pageService,
+      cdr,
+      authService,
+      dialog,
+      tupleService,
+      fileService,
+      modalService,
+      webcamService,
+      auth
+    );
+    const VIEW_OPTIONS = [
+      { pathname: 'llm', label: 'LLM', icon: 'psychology' },
+    ];
+    for (let i = 0; i < VIEW_OPTIONS.length; i++) {
+      const viewOption = VIEW_OPTIONS[i];
+      this.extraOptions.push({
+        action: () => {
+          this.router.navigate([viewOption.pathname]);
+        },
+        icon: viewOption.icon,
+        label: viewOption.label,
+      });
+    }
+  }
 
   override async ngOnInit() {
     await super.ngOnInit();
