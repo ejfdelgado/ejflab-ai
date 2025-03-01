4fc013876232d6bbea9cabb139670f5b19b8ac8a
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-03-01 12:22:05 -0500

    ui for db config

--- src/app/views/ejflabmodule/components/popup-database-edit/popup-database-edit.component.ts
+++ src/app/views/ejflabmodule/components/popup-database-edit/popup-database-edit.component.ts
@@ -33,21 +33,81 @@ export class PopupDatabaseEditComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     this.form = this.fb.group({
-      schemaName: ["", [Validators.required]],
-      tableName: ["", [Validators.required]],
+      schemaName: ["", []],
+      tableName: ["", []],
     });
+    this.refreshSchemas();
+  }
+
+  isNameValid(name: string) {
+    if (!/[a-z]+/.exec(name.trim())) {
+      this.modalSrv.alert({ title: "Opps!", txt: "Names only can contain a-z characters" });
+      return false;
+    }
+    return true;
+  }
+
+  async refreshSchemas() {
     this.schemas = await this.rACDatabaseSrv.getSchemas();
+    this.cdr.detectChanges();
+  }
+
+  async refreshTables() {
+    const name = this.form.get('schemaName')?.getRawValue();
+    if (!name) {
+      return;
+    }
+    this.tables = await this.rACDatabaseSrv.getTables(name);
+    this.cdr.detectChanges();
   }
 
   async selectSchema(schema: SchemaDataType) {
     this.form.get('schemaName')?.setValue(schema.name);
+    this.form.get('tableName')?.setValue('');
     // Reload table names
-    this.tables = await this.rACDatabaseSrv.getTables(schema.name);
+    this.refreshTables();
+  }
+
+  async selectTable(table: TableDataType) {
+    this.form.get('tableName')?.setValue(table.name);
     this.cdr.detectChanges();
   }
 
-  selectTable(table: TableDataType) {
+  async createSchema() {
+    const name = this.form.get('schemaName')?.getRawValue().trim();
+    if (this.isNameValid(name)) {
+      await this.rACDatabaseSrv.createSchema(name);
+      this.refreshSchemas();
+    }
+  }
 
+  async destroySchema() {
+    const name = this.form.get('schemaName')?.getRawValue();
+    if (name) {
+      const confirm = await this.modalSrv.confirm({ title: `Destroy ${name} schema?`, txt: "Can't be undone" });
+      if (confirm) {
+        await this.rACDatabaseSrv.destroySchema(name);
+        this.refreshSchemas();
+      }
+    }
+  }
+
+  async createTable() {
+    const schemaName = this.form.get('schemaName')?.getRawValue().trim();
+    const tableName = this.form.get('tableName')?.getRawValue().trim();
+    if (this.isNameValid(schemaName) && this.isNameValid(tableName)) {
+      await this.rACDatabaseSrv.createTable(schemaName, tableName);
+      this.refreshTables();
+    }
+  }
+
+  async destroyTable() {
+    const schemaName = this.form.get('schemaName')?.getRawValue().trim();
+    const tableName = this.form.get('tableName')?.getRawValue().trim();
+    if (this.isNameValid(schemaName) && this.isNameValid(tableName)) {
+      await this.rACDatabaseSrv.destroyTable(schemaName, tableName);
+      this.refreshTables();
+    }
   }
 
   cancel() {
--- src/app/views/ejflabmodule/services/racDatabase.service.ts
+++ src/app/views/ejflabmodule/services/racDatabase.service.ts
@@ -26,12 +26,36 @@ export class RACDatabaseService {
         return response;
     }
 
+    async createSchema(name: string): Promise<SchemaDataType[]> {
+        let response: SchemaDataType[] = [];
+        const data: any = await this.httpSrv.get(`srv/rac/db/schemas/${name}/create`);
+        return response;
+    }
+
+    async destroySchema(name: string): Promise<SchemaDataType[]> {
+        let response: SchemaDataType[] = [];
+        const data: any = await this.httpSrv.get(`srv/rac/db/schemas/${name}/destroy`);
+        return response;
+    }
+
     async getTables(schema: string): Promise<TableDataType[]> {
         let response: TableDataType[] = [];
         const data: any = await this.httpSrv.get(`srv/rac/db/schemas/${schema}/tables`);
         if (data.tables) {
             response = data.tables;
         }
+        return response;
+    }
+
+    async createTable(schemaName: string, tableName: string): Promise<SchemaDataType[]> {
+        let response: SchemaDataType[] = [];
+        const data: any = await this.httpSrv.get(`srv/rac/db/schemas/${schemaName}/tables/${tableName}/create`);
+        return response;
+    }
+
+    async destroyTable(schemaName: string, tableName: string): Promise<SchemaDataType[]> {
+        let response: SchemaDataType[] = [];
+        const data: any = await this.httpSrv.get(`srv/rac/db/schemas/${schemaName}/tables/${tableName}/destroy`);
         return response;
     }
 }
