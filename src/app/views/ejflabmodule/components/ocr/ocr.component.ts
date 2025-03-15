8d28dc9bd59d0b29232f29f754d73107726e478a
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-03-14 20:01:55 -0500

    it allows paste from clipboard

--- src/app/views/ejflabmodule/components/ocr/ocr.component.ts
+++ src/app/views/ejflabmodule/components/ocr/ocr.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { AuthorizationGetData, FileBase64Data, FlowchartProcessRequestData, FlowchartService, HttpService, ImagepickerOptionsData } from 'ejflab-front-lib';
 import { MyConstants } from '@ejfdelgado/ejflab-common/src/MyConstants';
 
@@ -34,12 +34,30 @@ export class OcrComponent {
 
   }
 
+  freeOldImage() {
+    if (this.currentSearchImage) {
+      try {
+        URL.revokeObjectURL(this.currentSearchImage);
+      } catch (err) { }
+    }
+  }
+
   async changedImage(imagenBase64: FileBase64Data) {
     if (imagenBase64.base64) {
       this.currentSearchBlob = await this.httpSrv.b64toBlob(
         imagenBase64.base64
       );
+      this.freeOldImage();
       this.currentSearchImage = URL.createObjectURL(this.currentSearchBlob);
+    }
+  }
+
+  async getImageFromClipBoard() {
+    const blob: Blob | null = await this.getClipboardImageBlob();
+    if (blob) {
+      this.currentSearchBlob = blob;
+      this.freeOldImage();
+      this.currentSearchImage = URL.createObjectURL(blob);
     }
   }
 
@@ -66,6 +84,42 @@ export class OcrComponent {
       const response = await this.flowchartSrv.process(payload, false);
 
       this.text = response.response.data.text;
+    }
+  }
+
+  async getClipboardImageBlob() {
+    try {
+      // Step 1: Read clipboard items
+      const clipboardItems = await navigator.clipboard.read();
+
+      // Step 2: Loop through clipboard items to find an image
+      for (const item of clipboardItems) {
+        for (const type of item.types) {
+          if (type.startsWith("image/")) {
+            // Step 3: Convert clipboard item to Blob
+            const blob = await item.getType(type);
+            return blob;
+          }
+        }
+      }
+      console.log("No image found in clipboard.");
+      return null;
+    } catch (error) {
+      console.error("Failed to get clipboard image:", error);
+      return null;
+    }
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    if (event.ctrlKey) {
+      switch (event.code) {
+        case 'KeyV':
+          this.getImageFromClipBoard();
+          break;
+        default:
+        //console.log(event.code);
+      }
     }
   }
 }
