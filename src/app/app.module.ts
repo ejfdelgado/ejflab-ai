8e0a6220bbfd87d5e3499fe17df49e737545ef5c
Edgar Delgado <edgar.jose.fernando.delgado@gmail.com>
2025-03-26 09:30:54 -0500

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -61,6 +61,7 @@ if (location.hostname == "localhost") {
     provideDatabase(() => getDatabase()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
+    { provide: 'appVersion', useValue: '1.3.6' },
     { provide: 'authProvider', useValue: 'google' },
     { provide: 'msTenant', useValue: '' },
     { provide: 'msClientId', useValue: '' },
